4dcf12d23680677e74103fd9c24941a32c4d766c
Mahdouch17 <mahdinouira17@gmail.com>
2021-11-22 00:47:52 +0100

    changes committed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
+import image from "./mahdi.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    Person: {
+      fullName: "Mohamed Mehdi Nouira",
+      bio: "I love coding",
+      imgSrc: image,
+      profession: "engineer",
+    },
+    show: false,
+  };
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <button style={{width:100,height:50,borderRadius:10,margin:40}} onClick={()=>{this.setState({show: !this.state.show})}}>Toggle</button>
+        {(this.state.show)&&(<div><img
+            style={{
+              width: 120,
+              height: 180,
+              border: "2px #1A5276 solid",
+              borderRadius: 15,
+            }}
+            src={this.state.Person.imgSrc}
+          />
+          <div style={{ fontSize: 24 }}>{this.state.Person.fullName}</div>
+          <div style={{ fontSize: 18 }}>{this.state.Person.bio}</div>
+          <div style={{ fontSize: 18 }}>{this.state.Person.profession}</div></div>)}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
